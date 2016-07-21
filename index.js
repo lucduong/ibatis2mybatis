e0fc2ac39788e5931da4b14c88236aea3f9d0f90
Luc <luc@e4u.vn>
2016-07-21 11:26:51 +0700

    Change port to 80

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(80);
 
 console.log("Running at Port 3000");
