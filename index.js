fb2a332f77062268e176087881608a6df4f93327
Luc <luc@e4u.vn>
2016-07-21 11:37:34 +0700

    Change port

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/vendor', express.static(__dirname + '/vendor'));
@@ -12,6 +13,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(80);
+app.listen(port);
 
-console.log("Running at Port 3000");
+console.log("Running at Port " + port);
